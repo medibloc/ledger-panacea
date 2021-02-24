c46380bb758b8c17c5e9b1e427a809d8fbb445e4
Youngjoon Lee <yjlee@medibloc.org>
2021-02-24 23:40:38 +0900

    Set 'Developed by: MediBloc'

--- deps/ledger-zxlib/app/common/view_x.c
+++ deps/ledger-zxlib/app/common/view_x.c
@@ -46,7 +46,7 @@ uint8_t flow_inside_loop;
 UX_FLOW_DEF_NOCB(ux_idle_flow_1_step, pbb, { &C_icon_app, MENU_MAIN_APP_LINE1, MENU_MAIN_APP_LINE2,});
 UX_STEP_CB_INIT(ux_idle_flow_2_step, bn,  h_expert_update(), h_expert_toggle(), { "Expert mode:", viewdata.value, });
 UX_FLOW_DEF_NOCB(ux_idle_flow_3_step, bn, { APPVERSION_LINE1, APPVERSION_LINE2, });
-UX_FLOW_DEF_NOCB(ux_idle_flow_4_step, bn, { "Developed by:", "Zondax.ch", });
+UX_FLOW_DEF_NOCB(ux_idle_flow_4_step, bn, { "Developed by:", "MediBloc", });
 UX_FLOW_DEF_NOCB(ux_idle_flow_5_step, bn, { "License:", "Apache 2.0", });
 UX_FLOW_DEF_VALID(ux_idle_flow_6_step, pb, os_sched_exit(-1), { &C_icon_dashboard, "Quit",});
 
